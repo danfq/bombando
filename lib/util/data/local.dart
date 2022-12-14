4717cdb4d8e6eed18fcca4494600f8a473a9199c
DanFQ <danfq.dev@gmail.com>
2022-12-14 01:33:22 +0000

    Added LocalData Methods

---
+++ lib/util/data/local.dart
@@ -0,0 +1,21 @@
+///Local Data
+class LocalData {
+  ///Open Boxes
+  static void openBoxes() {
+    //Boxes
+    final boxes = [
+      "preferences",
+    ];
+  }
+
+  ///Save Data
+  static void saveData({
+    required Map<String, String> data,
+  }) {}
+
+  ///Retrieve Data by setName & itemID
+  static void retrieveData({
+    required String setName,
+    required String itemID,
+  }) {}
+}
